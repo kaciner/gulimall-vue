22e8549d3550cdf9affafe49dac44b6910ef67f4
kaciner <yinzhe365@vip.qq.com>
2021-01-04 01:39:16 +0800

    为字段校验单独添加修改状态接口

--- src/views/modules/product/brand.vue
+++ src/views/modules/product/brand.vue
@@ -159,7 +159,7 @@
         let {brandId, showStatus} = data;
         // 发送请求修改状态
         this.$http({
-          url: this.$http.adornUrl('/product/brand/update'),
+          url: this.$http.adornUrl('/product/brand/update/status'),
           method: 'post',
           data: this.$http.adornData({brandId, showStatus}, false)
         }).then(({data}) => {
